da3f6d5766079324c7800ab348119b4cd4fda887
Ernesto Hegi <ernesto.hegi@gmail.com>
2017-01-31 14:00:44 +0000

    Organizes the code into an object.

--- hide-furigana.js
+++ hide-furigana.js
@@ -11,30 +11,61 @@
 (function() {
     'use strict';
 
-    var css = '.hide-furigana rt { color: transparent; }',
-        head = document.head || document.getElementsByTagName('head')[0],
-        style = document.createElement('style')
-    ;
+    var NHKHideFurigana = (function () {
+        'use strict';
 
-    style.type = 'text/css';
-    
-    if (style.styleSheet){
-        style.styleSheet.cssText = css;
-    } else {
-        style.appendChild(document.createTextNode(css));
-    }
+        var HIDE_FURIGANA_CLASS_NAME = 'hide-furigana',
+            NEWS_ARTICLE_ID_SELECTOR = 'newsarticle',
+            MOUSE_ENTER_EVENT_NAME   = 'mouseenter',
+            MOUSE_LEAVE_EVENT_NAME   = 'mouseleave'
+        ;
 
-    head.appendChild(style);
-    
-    var container = document.getElementById('newsarticle');
-   
-    container.classList.add('hide-furigana');
-    
-    container.addEventListener('mouseenter', function () {
-        container.classList.remove('hide-furigana');
-    });
-    
-    container.addEventListener('mouseleave', function () {
-        container.classList.add('hide-furigana');
-    });
+        var container;
+
+        var setElements = function () {
+            container = document.getElementById(NEWS_ARTICLE_ID_SELECTOR);
+        };
+
+        var appendStyle = function () {
+            var css = '.' + HIDE_FURIGANA_CLASS_NAME + ' rt { color: transparent; }',
+                head = document.head || document.getElementsByTagName('head')[0],
+                style = document.createElement('style')
+            ;
+
+            style.type = 'text/css';
+
+            if (style.styleSheet){
+                style.styleSheet.cssText = css;
+            } else {
+                style.appendChild(document.createTextNode(css));
+            }
+
+            head.appendChild(style);
+        };
+
+        var setContainerClass = function (className) {
+            container.classList.add(className);
+        };
+
+        var bindEvents = function () {
+            container.addEventListener(MOUSE_ENTER_EVENT_NAME, function () {
+                container.classList.remove(HIDE_FURIGANA_CLASS_NAME);
+            });
+
+            container.addEventListener(MOUSE_LEAVE_EVENT_NAME, function () {
+                container.classList.add(HIDE_FURIGANA_CLASS_NAME);
+            });
+        };
+
+        return {
+            init: function () {
+                appendStyle();
+                setElements();
+                setContainerClass(HIDE_FURIGANA_CLASS_NAME);
+                bindEvents();
+            }
+        };
+    })();
+
+    NHKHideFurigana.init();
 })();
